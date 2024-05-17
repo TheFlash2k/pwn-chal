c0717ad842d8ed39856436db6115a96193cb5b24
TheFlash2k <alitaqi2000@gmail.com>
2024-05-17 14:55:24 +0500

    Updated chal.exe

---
+++ utilities/chal.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+__attribute__((constructor))
+void __constructor__(){
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stderr, NULL, _IONBF, 0);
+}
+
+int main() {
+    puts("This is a test binary. Please replace this with your own.");
+    return 0;
+}
